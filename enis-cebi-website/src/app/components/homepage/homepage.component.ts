aa52b621e8fbb320947692a9bab2032377e8d69b
koseogluenes <enes.koseoglu@practicus.ai>
2024-11-27 23:12:06 +0300

    #2 A few images added. Home page layout changes. Services section.

--- enis-cebi-website/src/app/components/homepage/homepage.component.ts
+++ enis-cebi-website/src/app/components/homepage/homepage.component.ts
@@ -9,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './homepage.component.scss'
 })
 export class HomepageComponent {
-
+  services = [
+    { icon: 'bi-wifi', name: 'Ücretsiz Wi-Fi' },
+    { icon: 'bi-house-door', name: '24/7 Güvenlik' },
+    { icon: 'bi-cup-hot', name: 'Kafeterya' },
+    { icon: 'bi-tv', name: 'TV Odası' },
+    { icon: 'bi-person-arms-up', name: 'Spor Salonu' }, 
+    { icon: 'bi-basket', name: 'Çamaşırhane' }, 
+    { icon: 'bi-bus-front', name: 'Servis' },
+    { icon: 'bi-book', name: 'Kütüphane' },
+    { icon: 'bi-people', name: 'Sosyal Alan' },
+    { icon: 'bi-gear', name: 'Teknik Destek' }
+  ];
 }
