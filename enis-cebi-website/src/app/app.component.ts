42aaf5e37d85149defad4f89b2111c95dfa83cff
ozankocali <ozan.kocali@hotmail.com>
2024-11-30 17:37:49 +0300

    Home page components and footer added

--- enis-cebi-website/src/app/app.component.ts
+++ enis-cebi-website/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, RouterModule],
+  imports: [RouterOutlet, NavbarComponent, RouterModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
