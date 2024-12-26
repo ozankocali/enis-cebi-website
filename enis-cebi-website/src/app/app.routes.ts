804ffb7bfaf84757695a11fdf8b0a47ee3f2c774
ozankocali <ozan.kocali@hotmail.com>
2024-12-26 18:58:31 +0300

    about us component created

--- enis-cebi-website/src/app/app.routes.ts
+++ enis-cebi-website/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomepageComponent },
+    { path: 'about', component: AboutUsComponent },
 ];
