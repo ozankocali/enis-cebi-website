87f8633c451f9c370371af299746d7f841916ff3
ozankocali <ozan.kocali@hotmail.com>
2024-12-26 19:22:55 +0300

    Faciliries page initialized

--- enis-cebi-website/src/app/app.routes.ts
+++ enis-cebi-website/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
+import { FacilitiesComponent } from './components/facilities/facilities.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomepageComponent },
     { path: 'about', component: AboutUsComponent },
+    { path: 'facilities', component: FacilitiesComponent },
 ];
