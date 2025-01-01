73d80a8a297d034a49ff102fec6da606826e90b8
ozankocali <ozan.kocali@hotmail.com>
2025-01-01 16:14:21 +0300

    about us page added

--- enis-cebi-website/src/app/components/about-us/about-us.component.ts
+++ enis-cebi-website/src/app/components/about-us/about-us.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss'
 })
 export class AboutUsComponent {
+  screenWidth: number;
+  screenHeight: number;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.screenWidth = window.innerWidth;
+      this.screenHeight = window.innerHeight;
+    }
+    else {
+      this.screenWidth = 1000;
+      this.screenHeight = 1000;
+    }
+  }
+  ngOnInit(): void {
+    if (typeof document !== 'undefined') {
+      document.getElementById("modalButton")?.click();
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.screenWidth = event.target.innerWidth;
+    this.screenHeight = event.target.innerHeight;
+  }
 }
